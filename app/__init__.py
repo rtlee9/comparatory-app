7f4ac2d87bfd79a60d7c3029bc5c779e557afb7f
Ryan Lee <ryantlee9@gmail.com>
2016-09-24 17:59:31 -0400

    Add basic HTTP Basic Auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
-from flask import jsonify
 import psycopg2
 from elasticsearch import Elasticsearch
 from flask_bootstrap import Bootstrap
+from functools import wraps
+
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -32,6 +33,31 @@ es_aws = Elasticsearch(
     amazonaws.com/', verify_certs=True)
 
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'guest' and password == 'welcome'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     max_results = 10
@@ -49,6 +75,7 @@ def autocomplete():
 
 
 @app.route('/', methods=['GET', 'POST'])
+@requires_auth
 def index():
     errors = []
     results = []

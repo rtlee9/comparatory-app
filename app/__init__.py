bfb5b011b69b30389234be2b356aa12fd399cec4
Ryan Lee <ryantlee9@gmail.com>
2016-09-24 15:36:22 -0400

    Remove bug references

--- app/__init__.py
+++ app/__init__.py
@@ -116,5 +116,11 @@ def index():
     return render_template('index.html', errors=errors, results=results)
 
 
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+        return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run()

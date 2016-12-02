99f99898930eab65029a79175c5b8697b572e514
Ryan Lee <ryantlee9@gmail.com>
2016-12-02 00:39:48 -0500

    Connect app to Stormpath

--- app/comparatory.py
+++ app/comparatory.py
@@ -13,6 +13,7 @@ from bokeh.plotting import figure, ColumnDataSource
 from bokeh.models import HoverTool
 from bokeh.embed import components
 from flask_sslify import SSLify
+from flask_stormpath import StormpathManager
 
 
 app = Flask(__name__)
@@ -22,11 +23,19 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# AWS credentials
 AWS_RDS_HOST = os.environ['AWS_RDS_HOST']
 AWS_RDS_USER = os.environ['AWS_RDS_USER']
 AWS_RDS_PASSWORD = os.environ['AWS_RDS_PASSWORD']
 AWS_ES_ACCESS_KEY = os.environ['***REMOVED***']
 AWS_ES_SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+# OAuth credentials
+app.config['SECRET_KEY'] = os.environ['STORMPATH_SECRET_KEY']
+app.config['STORMPATH_API_KEY_ID'] = os.environ['STORMPATH_API_KEY_ID']
+app.config['STORMPATH_API_KEY_SECRET'] = os.environ['STORMPATH_API_KEY_SECRET']
+app.config['STORMPATH_APPLICATION'] = os.environ['STORMPATH_APPLICATION']
+stormpath_manager = StormpathManager(app)
 
 
 def set_scatter_data():
